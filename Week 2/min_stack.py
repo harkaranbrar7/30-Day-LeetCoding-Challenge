9c1d23b991539479b3596656bd5d3400eaf9e8ec
Harkaran Brar <harkaranbrar7@gmail.com>
2020-04-10 18:10:43 -0700

    min stack

---
+++ Week 2/min_stack.py
@@ -0,0 +1,178 @@
+'''
+  Min Stack
+Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+push(x) -- Push element x onto stack.
+pop() -- Removes the element on top of the stack.
+top() -- Get the top element.
+getMin() -- Retrieve the minimum element in the stack.
+ 
+
+Example:
+
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin();   --> Returns -3.
+minStack.pop();
+minStack.top();      --> Returns 0.
+minStack.getMin();   --> Returns -2.
+ 
+
+   Hide Hint #1  
+Consider each node in the stack having a minimum value. (Credits to @aakarshmadhavan)
+'''
+class MinStack1:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+
+    def push(self, x: int) -> None:
+        self.stack.insert(0,x)
+        
+
+    def pop(self) -> None:
+        self.stack.pop()
+        
+
+    def top(self) -> int:
+        return self.stack[0]
+        
+
+    def getMin(self) -> int:
+        return min(self.stack)
+
+class MinStack2:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: void
+        """
+        pre_min = 2147483647 if len(self.stack) == 0 else self.stack[-1][1]
+        cur_min = min(x, pre_min)
+        self.stack.append((x, cur_min))
+
+    def pop(self):
+        """
+        :rtype: void
+        """
+        self.stack.pop()
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self.stack[-1][0]
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        return self.stack[-1][1]
+
+class MinStack3:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.min = 2147483647
+        self.stack = []
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: void
+        """
+        if x <= self.min:
+            self.stack.append(self.min)
+            self.min = x
+        self.stack.append(x)
+
+    def pop(self):
+        """
+        :rtype: void
+        """
+        peak = self.stack.pop()
+        if peak == self.min:
+            self.min = self.stack.pop()
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self.stack[-1]
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        return self.min
+
+class MinStack4:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+        
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: void
+        """
+        if not self.stack:
+            self.stack.append((x, x))
+        else:
+            self.stack.append((x, min(x, self.stack[-1][1])))
+        
+
+    def pop(self):
+        """
+        :rtype: void
+        """
+        self.stack.pop()
+        
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self.stack[-1][0]
+        
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        return self.stack[-1][1]
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(x)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
+
+minStack = MinStack1()
+minStack.push(-2)
+minStack.push(0)
+minStack.push(-3)
+minStack.getMin()   
+minStack.pop()
+minStack.top()    
+minStack.getMin()   
